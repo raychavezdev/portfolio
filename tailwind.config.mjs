1306679d6368ea8e5f7a731ecb53689a7baf1a65
Raymundo <raymundo.developer@gmail.com>
2024-07-19 04:56:13 -0600

    Se agrega componente banner

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,18 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				'roboto': ['Roboto', 'sans-serif'],
+				'oswald': ['Oswald', 'sans-serif']
+			},
+			fontSize: {
+				'clamp': "clamp(3.5rem,8vw,15rem)"
+			},
+			backgroundImage: {
+				'grid': "repeating-linear-gradient(to right,black 0, black 1px,transparent 1px,transparent 20px),repeating-linear-gradient(to bottom,black 0, black 1px,transparent 1px,transparent 20px)"
+			}
+		},
 	},
 	plugins: [],
 }
