a9412b4c9b3ebc7edb6f243d7c7af88577984ac6
Raymundo Chavez <raymundo.developer@gmail.com>
2024-07-22 18:00:01 -0600

    Se agrega y se configura Eslint y Prettier

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+/** @type {import('prettier').Config} */
+module.exports = {
+    semi: false, // This is how you turn off semicolons, by the way
+    trailingComma: 'all',
+    singleQuote: true,
+    arrowParens: 'always',
+    jsxSingleQuote: false,
+    tabWidth: 2,
+    plugins: [require.resolve('prettier-plugin-astro'), require.resolve('prettier-plugin-tailwindcss')],
+    overrides: [{ files: '*.astro', options: { parser: 'astro' } }],
+    overrides: [{ files: ['*.json', '*.md', '*.toml', '*.yml'], options: { useTabs: false } }],
+}
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,19 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-			fontFamily: {
-				'roboto': ['Roboto', 'sans-serif'],
-				'oswald': ['Oswald', 'sans-serif']
-			},
-			fontSize: {
-				'clamp': "clamp(3.5rem,8vw,15rem)"
-			},
-			backgroundImage: {
-				'grid': "linear-gradient(#000 1px, transparent 1px),linear-gradient(90deg, #000 1px, transparent 1px)"
-			}
-		},
-	},
-	plugins: [],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
+        oswald: ['Oswald', 'sans-serif'],
+      },
+      fontSize: {
+        clampTitle: 'clamp(3.45rem,8vw,14.5rem)',
+        clamp: 'clamp(.75rem,2vw,.9rem)',
+        clampDes: 'clamp(.9rem,2vw,1rem)',
+      },
+      backgroundImage: {
+        grid: 'linear-gradient(#000 1px, transparent 1px),linear-gradient(90deg, #000 1px, transparent 1px)',
+      },
+    },
+  },
+  plugins: [],
 }
