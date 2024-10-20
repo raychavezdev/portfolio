d492daa7f49813f636c230a1b729aa05ab7597a1
Raymundo Chavez <raymundo.developer@gmail.com>
2024-10-20 12:16:56 -0600

    Agrega proyectos reales

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -8,7 +8,7 @@ export default {
         oswald: ['Oswald', 'sans-serif'],
       },
       fontSize: {
-        clampTitle: 'clamp(3.25rem,8vw,14.5rem)',
+        clampTitle: 'clamp(2.8rem,8vw,14.5rem)',
         clamp: 'clamp(.75rem,2vw,.9rem)',
         clampDes: 'clamp(.9rem,2vw,1rem)',
       },
